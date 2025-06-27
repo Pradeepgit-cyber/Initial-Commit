2d4b969c175fb6ffc5878362552083fad263d8ea
Pradeep Baraik <pradeep.kr.baraik@gmail.com>
2025-06-27 09:48:58 +0530

    New Learing

---
+++ PKBFile4.py
@@ -0,0 +1,29 @@
+import self
+from locust import HttpUser, between, task
+
+
+class user(HttpUser):
+    wait_time = between(1,5)
+    host = "https://test.api.hudle.in/"
+
+    @task
+    def Learning(self):
+
+
+         data = {
+                "name": "Jdjd",
+                "phone_number": "6540404044",
+                "sms_channel": "1",
+                "receive_updates": "1"
+              }
+         headers_auth ={
+    "Api-Secret": "hudle-api1798@prod",
+                "x-app-id": "TP1A.220905.001",
+                "Content-Type": "application/json"
+      }
+         with self.client.post("api/v1/register/consumer",json=data,headers=headers_auth) as response:
+                if response.status_code in (200,201):
+                    print("API Pass")
+
+                else:
+                    print("API fail")
